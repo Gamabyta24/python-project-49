a6943a25e29e320a50f7346b2b52b54d684c7245
Denaid24 <Gamabyta@gmail.com>
2024-08-23 19:14:15 +0300

    rework project , adding engin

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,41 +2,17 @@
 import prompt
 import random
 from brain_games.scripts.module import greetings, lose, congrats
-
+QUEST = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def is_even(num):
     return num % 2 == 0
 
 
-def main():
-    name = greetings()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    counter = 3
-    while True:
-        if counter == 0:
-            congrats(name)
-            break
-        quest_number = random.randint(1, 15)
-        print("question:", quest_number)
-        if quest_number % 2 == 0:
-            true_answer = "yes"
-            your_answer = prompt.string("Your answer: ")
-            if true_answer == your_answer:
-                print("Correct!")
-                counter -= 1
-            else:
-                lose(your_answer, true_answer, name)
-                break
-        else:
-            true_answer = "no"
-            your_answer = prompt.string("Your answer: ")
-            if true_answer == your_answer:
-                print("Correct!")
-                counter -= 1
-            else:
-                lose(your_answer, true_answer, name)
-                break
-
-
-if __name__ == "__main__":
-    main()
+def game_challange():
+    num = random.randint(1,30)
+    
+    if num % 2 == 0:
+        true_answer = 'yes'
+    else:
+        true_answer = 'no'
+    return num,true_answer
---
+++ brain_games/scripts/brain_even_play.py
@@ -0,0 +1,8 @@
+from brain_games.scripts import brain_even
+from brain_games.game_engine import run_game_engine
+
+def main():
+    run_game_engine(brain_even)
+
+if __name__ == "__main__":
+    main()
