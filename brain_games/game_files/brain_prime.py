3dee85a00604cbd3fc3339d74caff64fc95c2c87
Denaid24 <Gamabyta@gmail.com>
2024-08-24 21:26:10 +0300

    add brain-prime and asciinema

---
+++ brain_games/game_files/brain_prime.py
@@ -0,0 +1,7 @@
+import random
+QUEST = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+def game_challange():
+    num = random.randint(1,50)
+    is_prime_flag = num > 1 and all(num % i for i in range(2, int(num**0.5) + 1))
+    true_answer = "yes" if is_prime_flag else "no"
+    return num, true_answer
---
+++ brain_games/scripts/brain_prime_play.py
@@ -0,0 +1,10 @@
+from brain_games.game_files import brain_prime
+from brain_games.game_engine import run_game_engine
+
+
+def main():
+    run_game_engine(brain_prime)
+
+
+if __name__ == "__main__":
+    main()
